6927e1b76e7e2762b78ce4e306302d51ad7c965a
Brochu <alexbrochu1@gmail.com>
2024-05-03 22:13:55 -0400

    [SETUP] Use fabric template mod tool for starter code. Looking into understanding this base

---
+++ src/main/kotlin/com/broc/mcmod/McMod.kt
@@ -0,0 +1,24 @@
+package com.broc.mcmod
+
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.Item
+import net.minecraft.registry.Registry
+import net.minecraft.registry.Registries
+import net.minecraft.util.Identifier
+import org.slf4j.LoggerFactory
+
+object McMod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("mcmod")
+	private val custom_item = Item(FabricItemSettings())
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+		logger.info("[START] Preparing the mod")
+		Registry.register(Registries.ITEM, Identifier("mcmod", "custom_item"), custom_item)
+		logger.info("[DONE] Mod is prepped")
+	}
+}
---
+++ src/main/kotlin/com/broc/mcmod/McModDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.broc.mcmod
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object McModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
