2945452ef3c8f09d5ef0520b21aec8b17148208d
Brochu <alexbrochu1@gmail.com>
2024-05-05 03:15:51 -0400

    [ITEM] Implemented custom item with use override + item group

---
+++ src/main/kotlin/com/broc/mcmod/CustomItem.kt
@@ -0,0 +1,17 @@
+package com.broc.mcmod
+
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.sound.SoundEvents
+import net.minecraft.util.Hand
+import net.minecraft.util.TypedActionResult
+import net.minecraft.world.World
+
+class CustomItem(settings: Item.Settings) : Item(settings) {
+
+    override fun use(world: World?, user: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
+        user?.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0f, 1.0f)
+        return TypedActionResult.success(user?.getStackInHand(hand))
+    }
+}
--- src/main/kotlin/com/broc/mcmod/McMod.kt
+++ src/main/kotlin/com/broc/mcmod/McMod.kt
@@ -1,9 +1,15 @@
 package com.broc.mcmod
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.fabricmc.fabric.api.registry.FuelRegistry
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroups
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registry
 import net.minecraft.registry.Registries
+import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
@@ -12,9 +18,28 @@ object McMod : ModInitializer {
 	private val my_item = Registry.register(
 		Registries.ITEM,
 		Identifier("tutorial", "custom_item"),
-		Item(Item.Settings())
+		CustomItem(Item.Settings().maxCount(16))
 	)
 
 	override fun onInitialize() {
+		FuelRegistry.INSTANCE.add(my_item, 300)
+
+		// Add to current Item Group
+		//ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register {
+		//	content -> content.add(my_item)
+		//}
+
+		// Create new custom Item Group
+		val group = FabricItemGroup.builder()
+			.icon { ItemStack(my_item) }
+			.displayName(Text.translatable("itemGroup.tutorial.test_group"))
+			.entries { _, entries -> entries.add(my_item) }
+			.build()
+
+		Registry.register(
+			Registries.ITEM_GROUP,
+			Identifier("tutorial", "test_group"),
+			group
+		)
 	}
 }
