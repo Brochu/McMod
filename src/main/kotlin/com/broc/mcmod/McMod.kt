fdfc4f012d3c0fd845475d0144ea3d90e63d0072
Brochu <alexbrochu1@gmail.com>
2024-05-05 01:18:45 -0400

    [SETUP] Got a custom item in and working (with assets!!)

--- src/main/kotlin/com/broc/mcmod/McMod.kt
+++ src/main/kotlin/com/broc/mcmod/McMod.kt
@@ -1,7 +1,6 @@
 package com.broc.mcmod
 
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.item.Item
 import net.minecraft.registry.Registry
 import net.minecraft.registry.Registries
@@ -10,10 +9,12 @@ import org.slf4j.LoggerFactory
 
 object McMod : ModInitializer {
     private val logger = LoggerFactory.getLogger("mcmod")
+	private val my_item = Registry.register(
+		Registries.ITEM,
+		Identifier("tutorial", "custom_item"),
+		Item(Item.Settings())
+	)
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 	}
 }
