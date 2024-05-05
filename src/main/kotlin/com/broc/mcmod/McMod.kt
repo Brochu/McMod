0c1a8a8cea14306fd697e7a1b54bdc9cf5fb0b3b
Brochu <alexbrochu1@gmail.com>
2024-05-05 00:44:44 -0400

    [SETUP] Reset to check my understanding

--- src/main/kotlin/com/broc/mcmod/McMod.kt
+++ src/main/kotlin/com/broc/mcmod/McMod.kt
@@ -10,15 +10,10 @@ import org.slf4j.LoggerFactory
 
 object McMod : ModInitializer {
     private val logger = LoggerFactory.getLogger("mcmod")
-	private val custom_item = Item(FabricItemSettings())
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
-		logger.info("[START] Preparing the mod")
-		Registry.register(Registries.ITEM, Identifier("mcmod", "custom_item"), custom_item)
-		logger.info("[DONE] Mod is prepped")
 	}
 }
