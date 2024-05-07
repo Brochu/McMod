248d39146a5dc55b38393d2d528534bc61f20e0d
Brochu <alexbrochu1@gmail.com>
2024-05-07 01:56:42 -0400

    [TEST] Modified mixin to try and detect new day from ServerWorld::tickTime

--- src/main/java/com/broc/mcmod/mixin/ExampleMixin.java
+++ src/main/java/com/broc/mcmod/mixin/ExampleMixin.java
@@ -1,15 +1,21 @@
 package com.broc.mcmod.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(ServerWorld.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
+	@Inject(at = @At("HEAD"), method = "tickTime", cancellable = true)
+	private void tickTime(CallbackInfo ci) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
+		ActionResult res = NewDayCallback.EVENT.invoker().interact((ServerWorld)(Object)this);
+
+		if (res == ActionResult.FAIL) {
+			ci.cancel();
+		}
 	}
 }
