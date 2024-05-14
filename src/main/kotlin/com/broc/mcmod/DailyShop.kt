e241dca8b5d84e368741a5c6303f0e41591c6626
Brochu <alexbrochu1@gmail.com>
2024-05-14 01:48:21 -0400

    [SETUP] Create class to handle server wide shop offers, refreshes and status

---
+++ src/main/kotlin/com/broc/mcmod/DailyShop.kt
@@ -0,0 +1,13 @@
+package com.broc.mcmod
+
+/// Main class to handle creating currently active shop offers
+/// Make sure to reroll offers everyday
+/// Also Handles if the shop is open or closed based on time
+class DailyShop {
+
+    /// Configuration object for the Daily Shop
+    /// Parsing and access
+    class ShopConfig() {
+    }
+
+}
